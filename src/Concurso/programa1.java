2ad2a8592e08d6723d5c1db40a913a591c000bd0
rey emiliano <reyccastro74@gmail.com>
2025-11-26 07:44:20 -0600

    se agrego pagina incial

---
+++ src/Concurso/programa1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class programa1 {
+    public static void main(String[] args) {
+        Scanner tecla= new Scanner(System.in);
+        System.out.println("cual es el num de dias: ");
+        int dias=tecla.nextInt();
+
+        int[]  arreglo= new int[dias];
+        for(int i=0; i<dias; i++){
+            System.out.println("ingrese las horas dormidas en el dia: "); int horas=tecla.nextInt();
+
+        }
+        
+        
+    }
+}
+
+
+
